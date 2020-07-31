5508f2775239cdbcc4094ab6e945923e0936ba10
SHEIKH ZAIN <68387758+sheikhzain99@users.noreply.github.com>
2020-07-31 17:07:18 +0530

    login

--- accounts/views.py
+++ accounts/views.py
@@ -67,3 +67,7 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')       
+def forgotpassword(request):
+    return render(request,'atoz_store/forgotpassword.html')
+    
+
